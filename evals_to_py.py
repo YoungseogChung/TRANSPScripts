97c80ebb39690c218ad3da5b5451d0bdf934cd30
IanChar <ianchar4@gmail.com>
2018-10-11 15:43:23 -0400

    Added beta extraction

--- evals_to_py.py
+++ evals_to_py.py
@@ -4,23 +4,28 @@ Script to manage .out files.
 import sys
 import numpy as np
 from os import listdir
+import argparse
 
 PLASMA_EVAL_COL_INDEX = 1
 TOTAL_EVAL_HEADER = 'Total Energy Eigenvalues:'
 TOTAL_EVAL_INDEX = 3
 
-def extract_dir_evals(dir_path, out_path=None):
+BETA = 'beta'
+
+def extract_dir_info(dir_path, out_path=None, betas=False):
     """
-    Extracts evecs where each column is a evec and evecs are sorted from left
-    to right. One unfortunate thing is that there seem to be varying amounts
-    of evals for some reason. If evals are missing fill in 0s at the bottom.
+    Extracts evecs (or beta vals) where each column is a evec and evecs
+    are sorted from left to right. One unfortunate thing is that there seem
+    to be varying amounts of evals for some reason. If evals are missing
+    fill in 0s at the bottom.
     """
     files = listdir(dir_path)
     files.sort()
     cols = []
+    extract_vec = _betas_to_vec if betas else _evecs_to_vec
     for col, f in enumerate(files):
         f_path = ''.join([dir_path, '/', f])
-        cols.append(_evecs_to_vec(f_path))
+        cols.append(extract_vec(f_path))
     eval_mat = np.zeros((max([len(col) for col in cols]), len(files)))
     for col_ind, col in enumerate(cols):
         eval_mat[:len(col),col_ind] = col
@@ -54,9 +59,31 @@ def _evecs_to_vec(path):
         raise ValueError('Invalid format of file, no evals found: %s' % path)
     return np.asarray(evals)
 
+def _betas_to_vec(path):
+    """
+    Extract the beta values in the output and return as a vector.
+    """
+    f = open(path, 'r')
+    lines = f.readlines()
+    f.close()
+    beta_line_ind = 0
+    for line in lines:
+        beta_line_ind += 1
+        headers = line.split()
+        if len(headers) > 0 and BETA in headers[0]:
+            beta_line_ind += 1
+            break
+    if beta_line_ind == len(lines):
+        raise ValueError('Could not find beta header in file: %s' % path)
+    return np.asarray([float(n) for n in lines[beta_line_ind].split()])
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        raise ValueError('Incorrect number of command line args.')
-    src_dir = sys.argv[1]
-    out_path = sys.argv[2]
-    print extract_dir_evals(src_dir, out_path)
+    parser = argparse.ArgumentParser('Script for extracting DCON output info')
+    parser.add_argument('src_dir', type=str,
+                        help='Path to directory of DCON output')
+    parser.add_argument('dest_path', type=str,
+                        help='Path of .npy file to write result to.')
+    parser.add_argument('--beta', action='store_true',
+                        help='Extract beta values')
+    args = parser.parse_args()
+    print extract_dir_info(args.src_dir, args.dest_path, betas=args.beta)
